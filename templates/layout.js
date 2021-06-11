a5a9cb26e611de328759535926b307116fdc30e1
yusuke1997 <63439062+yusuke1997@users.noreply.github.com>
2021-06-12 01:39:07 +0900

    fixed

--- templates/layout.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var $textarea = $('#textarea');
-    var lineHeight = parseInt($textarea.css('lineHeight'));
-    $textarea.on('input', function(e) {
-      var lines = ($(this).val() + '\n').match(/\n/g).length;
-      $(this).height(lineHeight * lines);
-    });
-  });
